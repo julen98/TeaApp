41ae324751b1de9788d7a549583e07540902487d
Julen <julenramon792@gmail.com>
2021-11-18 12:57:34 +0100

    First

---
+++ android/app/src/main/kotlin/com/example/dashboard_tea/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dashboard_tea
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: <Widget> [
+          Container(
+            height: size.height * .3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                alignment:Alignment.topCenter,
+                image: AssetImage('assets/top_header.png')
+              )
+            )
+          ),
+
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 64,
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget> [
+                        CircleAvatar(
+                          radius: 32,
+                          backgroundImage: NetworkImage('https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg'),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('Juan Andrés'),
+                            Text('410410414001'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Expanded(
+                   child: GridView.count(
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      primary: false,
+                      crossAxisCount: 2,
+                      children: <Widget>[
+                        Card(
+                          child: InkWell(
+                            onTap: (){},
+                            splashColor: Colors.green,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset('home.png', height: 128,),
+                                Text('Home'),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Card(
+                          child: InkWell(
+                            onTap: (){},
+                            splashColor: Colors.green,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset('shop.png', height: 128,),
+                                Text('Shop'),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Card(
+                          child: InkWell(
+                            onTap: (){},
+                            splashColor: Colors.green,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset('timer.png', height: 128,),
+                                Text('Timer'),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Card(
+                          child: InkWell(
+                            onTap: (){},
+                            splashColor: Colors.green,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset('settings.png', height: 128,),
+                                Text('Settings'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
